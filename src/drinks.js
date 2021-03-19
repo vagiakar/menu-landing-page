dff58eb3861032624953f5bbf3c4d1a29bed28d6
vagiakar <vagiakaraba@gmail.com>
2021-03-19 19:46:29 +0200

    display different menu based on chosen category

---
+++ src/drinks.js
@@ -0,0 +1,146 @@
+const drinks = [
+  {
+    category: "Coffees",
+    temperature: "hot",
+    name: "Cappuccino",
+    price: 1.8,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "../images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "hot",
+    name: "Espresso",
+    price: 1.4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "../images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "hot",
+    name: "Macchiato",
+    price: 1.6,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "../images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "hot",
+    name: "Cappuccino latte",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "../images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "cold",
+    name: "Espresso freddo",
+    price: 1.8,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "cold",
+    name: "Iced latte",
+    price: 2.3,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "cold",
+    name: "Freddo cappuccino",
+    price: 2.0,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Coffees",
+    temperature: "cold",
+    name: "Greek coffee",
+    price: 1.2,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Non-coffees",
+    temperature: "hot",
+    name: "Hot chocolate",
+    price: 1.8,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Non-coffees",
+    temperature: "cold",
+    name: "Iced chocolate",
+    price: 1.8,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Non-coffees",
+    temperature: "cold",
+    name: "Orange juice",
+    price: 2.3,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Non-coffees",
+    temperature: "hot",
+    name: "Tea",
+    price: 2.3,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Non-coffees",
+    temperature: "cold",
+    name: "Iced tea",
+    price: 2.3,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Desserts",
+    temperature: null,
+    name: "Cupcake",
+    price: 1.5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Desserts",
+    temperature: null,
+    name: "Croissant",
+    price: 1.0,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+  {
+    category: "Desserts",
+    temperature: null,
+    name: "Cheesecake",
+    price: 2.0,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, mollitia.",
+    image: "images/coffee-item.jpg",
+  },
+];
+export default drinks;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import drinks from "../drinks.js";
 import img from "../images/coffee-item.jpg";
+const coffees = drinks.filter((drink) => {
+  return drink.category === "Coffees";
+});
+const nonCoffees = drinks.filter((drink) => {
+  return drink.category === "Non-coffees";
+});
+const desserts = drinks.filter((drink) => {
+  return drink.category === "Desserts";
+});
 
 export default function Menu() {
+  const [category, setCategory] = useState("All");
+  const [selectedDrinks, setSelectedDrinks] = useState([
+    coffees,
+    nonCoffees,
+    desserts,
+  ]);
+
+  useEffect(() => {
+    changeMenu();
+  }, [category]);
+
+  function changeMenu() {
+    let drinks = [];
+    if (category === "Coffees") {
+      drinks = [coffees];
+    } else if (category === "Non-coffees") {
+      drinks = [nonCoffees];
+    } else if (category === "Desserts") {
+      drinks = [desserts];
+    } else {
+      drinks = [coffees, nonCoffees, desserts];
+    }
+    setSelectedDrinks(drinks);
+  }
+
   return (
     <>
       <section className="menu-header">
@@ -11,152 +46,60 @@ export default function Menu() {
       <div className="container">
         <section className="menu">
           <div className="menu-buttons-container">
-            <button className="btn menu-btn active">All</button>
-            <button className="btn menu-btn">Cappuccino</button>
-            <button className="btn menu-btn">Espresso</button>
-            <button className="btn menu-btn">Greek</button>
-            <button className="btn menu-btn">Tea</button>
-            <button className="btn menu-btn">Chocolate</button>
+            <button
+              className={`btn menu-btn  ${category === "All" && "active"}`}
+              onClick={() => setCategory("All")}
+            >
+              All
+            </button>
+            <button
+              className={`btn menu-btn  ${category === "Coffees" && "active"}`}
+              onClick={() => setCategory("Coffees")}
+            >
+              Coffees
+            </button>
+            <button
+              className={`btn menu-btn ${
+                category === "Non-coffees" && "active"
+              }`}
+              onClick={() => setCategory("Non-coffees")}
+            >
+              Non-coffees
+            </button>
+            <button
+              className={`btn menu-btn ${category === "Desserts" && "active"}`}
+              onClick={() => setCategory("Desserts")}
+            >
+              Desserts
+            </button>
           </div>
           <div className="menu-display">
-            <div className="category">
-              <h2>Espresso</h2>
-              <div className="menu-cards-container">
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
+            {selectedDrinks.map((category) => {
+              return (
+                <div className="category">
+                  <h2>{category[0].category}</h2>
+                  <div className="menu-cards-container">
+                    {category.map((drink) => {
+                      return (
+                        <div className="suggest-card">
+                          <Link to="/item">
+                            <img
+                              className="suggest-card-img"
+                              src={img}
+                              alt="coffee"
+                            />
+                            <p className="suggest-card-txt  menu-card-txt">
+                              {drink.name}
+                              <span className="price">{` ${drink.price} €`}</span>
+                            </p>
+                          </Link>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="category">
-              <h2>Espresso</h2>
-              <div className="menu-cards-container">
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-                <div className="suggest-card">
-                  <Link to="/item">
-                    <img className="suggest-card-img" src={img} alt="coffee" />
-                    <p className="suggest-card-txt  menu-card-txt">
-                      Cappuccino Latte <span className="price">2.5$</span>
-                    </p>
-                  </Link>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </section>
       </div>
