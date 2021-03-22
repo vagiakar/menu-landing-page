8b1e82cbceebbbf46ba245a8dae7b6a10aad7548
vagiakar <vagiakaraba@gmail.com>
2021-03-22 10:59:26 +0200

    correct double border in navbar bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,42 @@ export default function Navbar() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const refNavbarList = useRef();
   const refNavbarListContainer = useRef();
+  const refDivider = useRef();
   function displayNavbar() {
     setIsNavbarOpen(!isNavbarOpen);
   }
   useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
     const linksHeight = refNavbarList.current.getBoundingClientRect().height;
     if (isNavbarOpen) {
       refNavbarListContainer.current.style.height = `${linksHeight}px`;
+      const borderColor = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue("--color-gray");
+      refDivider.current.style.visibility = `visible`;
     } else {
       refNavbarListContainer.current.style.height = `0px`;
+      refDivider.current.style.visibility = `hidden`;
     }
   }, [isNavbarOpen]);
+
+  function handleResize() {
+    if (window.innerWidth > 800) {
+      setIsNavbarOpen(false);
+    }
+  }
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
         Coffee<br></br>Shop
       </Link>
-      <div className="divider"></div>
+      <div className="divider" ref={refDivider}></div>
       <div className="navbar-list-container" ref={refNavbarListContainer}>
         <ul className="navbar-list" ref={refNavbarList}>
           <li className="navbar-item">
